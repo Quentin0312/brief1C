2a56fc06984995151ce35cbcd99d9d0e56659c40
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2022-11-23 12:03:51 +0400

    test raw query en cours

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Factures(models.Model):
+    nofacture = models.IntegerField(primary_key=True)
+    datefacturation = models.DateField(blank=True, null=True)
+    region = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'factures'
+
+class Produits(models.Model):
+    codeproduit = models.CharField(primary_key=True, max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'produits'
+
+class Contenir(models.Model):
+    nofacture = models.OneToOneField('Factures', models.DO_NOTHING, db_column='nofacture', primary_key=True)
+    codeproduit = models.ForeignKey('Produits', models.DO_NOTHING, db_column='codeproduit')
+    qte = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'contenir'
+        unique_together = (('nofacture', 'codeproduit'),)
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Factures, Produits, Contenir
+
 # Create your views here.
-
+test =[]
 def mainDashboard(request):
+    for elt in Produits.objects.raw('SELECT * FROM produits'):
+        test.append(elt) 
+    print(test)
 
-    return HttpResponse("hello world")
+    return HttpResponse(test)

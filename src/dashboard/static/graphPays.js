f03fabe296ccf7ef78a54ac76d7a8980dade22e2
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2022-11-30 14:26:13 +0400

    graph camembert fonctionel js presque completement hors HTML

--- src/dashboard/static/graphPays.js
+++ src/dashboard/static/graphPays.js
@@ -1,21 +1,21 @@
 const config = {
     type: 'pie',
     data: {
-      datasets: [{
-        data: {{ data|safe }},
+        datasets: [{
+        data: data2,
         backgroundColor: [
-          '#696969', '#808080', '#A9A9A9', '#C0C0C0', '#D3D3D3'
+            '#696969', '#808080', '#A9A9A9', '#C0C0C0', '#D3D3D3'
         ],
         label: 'Population'
-      }],
-      labels: {{ labels|safe }}
+        }],
+        labels: labels2
     },
     options: {
-      responsive: true
+        responsive: true
     }
-  };
+    };
 
-  window.onload = function() {
+    window.onload = function() {
     var ctx = document.getElementById('pie-chart').getContext('2d');
     window.myPie = new Chart(ctx, config);
-  };
+    };

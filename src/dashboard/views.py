68a305317e2c4c2b84b20089c0a1929874bcd316
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2022-12-01 17:44:15 +0400

    dashboard views.py cleaned

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-from django.db import connections
 
 from . forms import UploadFileForm
 
 from .models import Factures, Produits, Contenir
+from django.db import connections
 
 import pandas as pd
 
-from django_pivot.pivot import pivot
 
 # Fonctions
 def csvToBDD(dataframe):
@@ -103,37 +102,15 @@ def selectSQL(scriptSQL):
 # Views
 def mainDashboard(request):
 
-    # for elt in Factures.objects.raw('SELECT * FROM factures'):
-    #     test = elt.region
-    #---------------------------------------------------------------------------------------------
-    # date = '2010-01-12'
-    # region ='United Kingdom'
-    # stockcode = '85123A'
-    # invoiceno = 536365
-    # invoicedate = '2010-01-12'
-    # country = 'United Kingdom'
-    # qte = 6
-
-    # cursor = connections['default'].cursor()
-    # cursor.execute("INSERT INTO produits(codeproduit) VALUES( %s )", [stockcode])
-    # cursor.execute("INSERT INTO factures(nofacture,datefacturation,region) VALUES( %s , %s , %s )", [invoiceno, invoicedate, country])
-    # cursor.execute("INSERT INTO contenir(nofacture,codeproduit,qte) VALUES ( %s , %s , %s )",[invoiceno, stockcode, qte])
-    #---------------------------------------------------------------------------------------------
-
     return render(request,"mainDashboard.html")
 
-# découper l'interieur en fonctions
 def graphPays(request):
 
     # Requette SQL
-    cursor = connections['default'].cursor()
-    # TOP 10 seulement
-    cursor.execute("SELECT factures.region, COUNT(*) AS vente FROM factures INNER JOIN contenir ON factures.nofacture = contenir.nofacture GROUP BY factures.region ORDER BY vente DESC LIMIT 5")
-    rows = cursor.fetchall()
-    print(rows)
-    print(type(rows))
+        # TOP 5 
+    rows = selectSQL("SELECT factures.region, COUNT(*) AS vente FROM factures INNER JOIN contenir ON factures.nofacture = contenir.nofacture GROUP BY factures.region ORDER BY vente DESC LIMIT 5")
 
-    # row to variable
+    # Récupération datasets pour chart JS
     valeurs, labels = rowToVariable(rows)
 
     # Context
@@ -147,10 +124,10 @@ def graphPays(request):
 def graphProduits(request):
 
     # Requete SQL
-    cursor = connections['default'].cursor()
-    cursor.execute("SELECT codeproduit, COUNT(*) AS vente FROM contenir GROUP BY codeproduit ORDER BY vente DESC LIMIT 10")
-    rows = cursor.fetchall()
+        # TOP 10
+    rows = selectSQL("SELECT codeproduit, COUNT(*) AS vente FROM contenir GROUP BY codeproduit ORDER BY vente DESC LIMIT 10")
 
+    # Récuperation datasets pour chart JS
     valeurs, labels = rowToVariable(rows)
 
     # Context
@@ -162,6 +139,7 @@ def graphProduits(request):
     return render(request, "graphProduits.html", context)
 
 def upload_file(request):
+    # Etape 2: Situation POST
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         file = request.FILES['file'] # Juste à partir de la le script peut se lancer ? Car pas besoin de save le fichiers csv juste l'enregistrer en pandas dataframe ?
@@ -169,10 +147,12 @@ def upload_file(request):
         # Debut "script" d'importation
         df = pd.read_csv(file)
         # Ici aplliquer fonction de nettoyage qui renvoie un dataframe à reutiliser juste après
+        
+        # Importation dans la BDD
         csvToBDD(df)
 
         return HttpResponse("Nom du fichiers: "+str(df))
-    # Etape 1 : Ouverture du lien => GET
+    # Etape 1
     else:
         form = UploadFileForm()
     return render(request, 'add.html', context={'form':form}) #Actualise la page en ajoutant le form file en context

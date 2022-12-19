5cdade66332a3e2dc74cf630f357024fc199c264
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2022-12-19 15:02:28 +0400

    probleme de redirection url vide corrigé

--- src/brief1C/urls.py
+++ src/brief1C/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
-    # path('account/', include('account.urls')),
-    path('', include("django.contrib.auth.urls"))
+    path('', include("django.contrib.auth.urls")),
+    path('', RedirectView.as_view(pattern_name='mainDashboard'), name='old_view')
 ]
